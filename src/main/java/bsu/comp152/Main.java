e4894a4bc80a9775f363ae4857262507f7575911
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-04-19 07:53:12 -0400

    Update Main.java

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,16 +1,21 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import java.util.Random;
 
 public class Main extends Application {
-
     public static void main(String[] args) {
 
         // launch the application
@@ -19,41 +24,38 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage){
+        // Create a label
+        Label enterColor = new Label("Enter a color:");
+        enterColor.setFont(Font.font(18));
+        // Create TextField
+        TextField textField = new TextField(); // Book page num 787
 
-        // Create a label component.
-        Label messageLabel = new Label("Hello World");
 
-        // Put the label in an HBox layout container.
-        HBox hbox = new HBox(messageLabel);
+        // Create Button
+        Button button = new Button("Change Color");
 
-        // Put the label in a Scene.
-        // Scene scene = new Scene(messageLabel);
-        // Or make the scene 500 pixels wide by 200 pixels high.
-        // Create a Scene with the HBox as its root node.
-        Scene scene = new Scene(hbox, 500, 200);
+        //Create Circle
+        Circle circle = new Circle(20, Paint.valueOf("Black"));
 
-        Random rand = new Random();
-        int r = rand.nextInt(3);
+        //Create a Hbox
+        HBox moveColorText = new HBox(enterColor, textField, button, circle);
 
-        if (r==0){
-            // Set the alignment of the HBox to center.
-            hbox.setAlignment(Pos.CENTER_LEFT);
-        }
-        else if (r == 1){
-            hbox.setAlignment(Pos.CENTER);
-        }
 
-        else{
-            hbox.setAlignment(Pos.CENTER_RIGHT);
-        }
+        // Setup Hbox
+        Scene scene = new Scene(moveColorText, 400, 300);
+        moveColorText.setAlignment(Pos.TOP_LEFT);
+        moveColorText.setSpacing(20);
 
-        // Add the Scene to the Stage.
+
+        // Add scene to the stage
         primaryStage.setScene(scene);
 
-        // Set the window's title.
-        primaryStage.setTitle("My First GUI Application");
+        // Set the stage title
+        primaryStage.setTitle("Color changer");
 
-        // Show the window.
+        // Show the window
         primaryStage.show();
+
     }
+
 }
